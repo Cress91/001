55d677440e78c952f5af3c1fa429d7c6028928e4
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-05 16:21:48 +0300

    first commit

---
+++ 001/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход
+// трехзначное число и показывает вторую цифру этого числа
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = (number1 / 10) % 10;
+
+System.Console.WriteLine(number2);
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+int number = int.Parse(Console.ReadLine());
+
+int Funk(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum = number %10;
+        number = number / 10;
+    }
+    return sum;
+}
+Console.WriteLine(Funk(number));
