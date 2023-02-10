2bbdb41d90a083f035de4401cf7d62788a5ffbfb
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-10 19:29:07 +0300

    New commit

---
+++ 001/Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает число 
+// N и выдает таблицу кубов чисел от 1 до N
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(i * i * i);
+}
