bf129ca069122e8b804187a3e49de52c907df311
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-10 16:00:52 +0300

    new commit

---
+++ 001/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает 5-значное
+// число и проверяет, является ли оно палиндромом
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num / 10000 != num % 10)
+{
+    System.Console.WriteLine("No");
+}
+   if ((num / 1000) % 10 != (num % 100) / 10)
+   {
+    System.Console.WriteLine("No");
+   }
+else
+{
+    System.Console.WriteLine("Yes");
+}
