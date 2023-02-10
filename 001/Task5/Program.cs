63de2899bf4493b5f1be61bc7ece220bffdd1d52
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-10 19:02:17 +0300

    New commit

---
+++ 001/Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает 2 точек
+// и находит расстояние между ними в 3д
+
+System.Console.WriteLine("Введите число");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int z2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+
+ Console.WriteLine(d); 
