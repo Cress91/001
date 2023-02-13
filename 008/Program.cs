c93ac11f4557d227061ee4cf3961c2f4c4557078
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-14 01:47:33 +0300

    New commit

---
+++ 008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает
+// число и выдает сумму цифр в числе.
+
+System.Console.WriteLine((Введите число));
+int num = int.Parse(Console.ReadLine());
+
+int  Func(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+       sum = sum + num % 10
+       num = num / 10
+    
+       }   return sum;
+
+}
+System.Console.WriteLine(Func((num)));
