c4b841eadf7036fe5a117ed4fe787dac85366875
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-14 01:00:14 +0300

    New commit

---
+++ Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает 2 числа А и В
+// и возводит число А в натуральную степень В.
+
+System.Console.WriteLine(("Введите число А"));
+int num = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите число В");
+int num1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine(Expon(num, num1));
+
+
+int Expon(int a, int x)
+{
+    int result = 1;
+    for (int i = 0; i < x; i++)
+    {
+        result *= a;
+    }
+
+}
