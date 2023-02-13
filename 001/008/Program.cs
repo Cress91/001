c95bf7714301ea1270dfbef3b3b52bd7a796a210
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-14 02:54:25 +0300

    New commitS

---
+++ 001/008/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит массив 
+// из 8 эл-тов и выводит их на экран
+
+PrintArray(GetRandomArray(8));
+
+int[] GetRandomArray(int legth)
+{
+    int[] result = new int[legth];
+
+    for (int i = 0; i < legth; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.WriteLine(item);
+    }
+}
+
