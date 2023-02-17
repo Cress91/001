276311be446ab912b84986b1b585ea7c9689a2af
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-17 16:52:53 +0300

    New commit

---
+++ Task9/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов стоящих на нечетных позициях.
+
+System.Console.WriteLine("Input the size of the array");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int[] array = GetArray(size);
+
+int[] array (int size)
+{
+   int[] array = new int[size];
+
+   for (int i = 0; i < size; i++)
+   {
+       array[i] = new Random().Next(-100, 100);
+       int element = array[i];
+       if (i % 2 != 0)
+       {
+        sum = sum + element;
+       }
+   }
+   return array;
+}
+System.Console.WriteLine(String.Join(",", array));
+System.Console.WriteLine($"Sum of elements of odd numbers: {sum}");
