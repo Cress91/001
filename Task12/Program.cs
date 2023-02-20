6ed5cb2e4df30bdcacbbf4b6cfdf9a93bccc3640
Cress91 <116718941+Cress91@users.noreply.github.com>
2023-02-20 22:45:48 +0300

    New commit

---
+++ Task12/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая найдет точку пересечения 2х прямых,
+// заданных уравнениями у = к1 * х + в1, у = к2 * х + в2;
+// значения в1, к1, в2, к2 задаются пользователем.
+// в1 = 2, к1 = 5, в2 = 4, к2 = 9 ->(-0,5; -0,5)
+
+System.Console.WriteLine("The aquation is y = kx + b");
+
+System.Console.WriteLine("Enter k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Enter b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2)
+{
+    System.Console.WriteLine("The lines are parallel");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1*x + b1;
+
+    System.Console.WriteLine($"The solvation is ({x}; {y})");
+}
